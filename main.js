f6092962c8c28d1894c1117e62cd504d3f68cb80
harryge00 <harryge00@gmail.com>
2017-12-03 22:34:46 +0800

    use queue

--- main.js
+++ main.js
@@ -3,65 +3,84 @@ var sleep = require('sleep');
 const base_url =
   "https://www.okex.com/api/v1/future_ticker.do";
 var lastPrices = {
-	"btc": 10000,
-	"ltc": 100,
-	"etc": 30,
-	"eth": 450,
+    "btc": 10000,
+    "ltc": 100,
+    "etc": 30,
+    "eth": 450,
+	"bch": 1700
+}
+
+var priceRecords = {
+	"btc": [],
+	"ltc": [],
+	"etc": [],
+	"eth": [],
+	"bch": []
 }
 var nextCoin = {
 	"btc": "ltc",
 	"ltc": "etc",
 	"etc": "eth",
-	"eth": "btc"
+	"eth": "bch",
+	"bch": "btc"
+}
+
+var records_length = 70 / 0.5;
+//
+function checkFluctuation(array, threshold){
+	var min = 99999;
+	var max = 0;
+	for(var i = 0; i < array.length; i++) {
+		if(array[i] > max) {
+			max = array[i];
+		}
+		if(array[i] < min) {
+			min = array[i];
+		}
+	}
+	if((max - min) / array[array.length - 1] > threshold) {
+		return true;
+	}
+	return false;
 }
 
 function checkPrice(coin, contract_type, threshold) {
-
 	var url = base_url + "?symbol=" + coin + "_usd&contract_type=" + contract_type;
-	console.log("checking", coin, contract_type, threshold, url);
+	// console.log("checking", coin, contract_type, threshold, url);
 	request.get(url, (error, response, body) => {
 		if(error) {
 			console.error(error);
-			sleep.msleep(200);
+			sleep.msleep(100);
 			checkPrice(nextCoin[coin], contract_type, threshold);
 			return
 		}
+		// console.log(body);
 		body = JSON.parse(body);
-		if(coin == "etc") {
-			console.log(lastPrices);
-			console.log(body);
+		priceRecords[coin].push(body.ticker.last);
+		if(priceRecords[coin].length >= records_length) {
+			var flag = checkFluctuation(priceRecords[coin]);
+			if(flag) {
+				console.log("Alarm: ", coin);
+				console.log(priceRecords[coin]);
+			}
+			priceRecords[coin].shift();
+			console.log(priceRecords[coin]);
 		}
-		if((body.ticker.last - lastPrices[coin])/ lastPrices[coin] >= threshold) {
-			console.log("Alarm: ", coin, " price has surged!");
-			console.log(lastPrices[coin]);
-			console.log(body);
-		} else if((body.ticker.last - lastPrices[coin])/ lastPrices[coin] <= -threshold) {
-			console.log("Alarm: ", coin, " price has failed!");
-			console.log(lastPrices[coin]);
-			console.log(body);
-		}
-		lastPrices[coin] = body.ticker.last;
-		sleep.msleep(200);
+		// body.ticker.last 
+		// if((body.ticker.last - lastPrices[coin])/ lastPrices[coin] >= threshold) {
+		// 	console.log("Alarm: ", coin, " price has surged!");
+		// 	console.log(lastPrices[coin]);
+		// 	console.log(body);
+		// } else if((body.ticker.last - lastPrices[coin])/ lastPrices[coin] <= -threshold) {
+		// 	console.log("Alarm: ", coin, " price has failed!");
+		// 	console.log(lastPrices[coin]);
+		// 	console.log(body);
+		// }
+		// lastPrices[coin] = body.ticker.last;
+		sleep.msleep(100);
 		checkPrice(nextCoin[coin], contract_type, threshold);
 	});
 
 }
 
 checkPrice("btc", "quarter", 0.005);
-
-// function main() {
-// 	var threshold = 0.005;
-// 	var contract_type = "quarter";
-// 	while(1) {
-// 		console.log("checking");
-// 		checkPrice("btc", contract_type, threshold);
-// 		sleep.msleep(150);
-// 		checkPrice("ltc", contract_type, threshold);
-// 		sleep.msleep(150);
-// 		checkPrice("etc", contract_type, threshold);
-// 		sleep.msleep(150);
-// 		checkPrice("eth", contract_type, threshold);
-// 		sleep.msleep(1500);
-// 	}
-// }
-// main();
